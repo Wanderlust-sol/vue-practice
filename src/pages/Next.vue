9c30e7df83002f6b446bf5b10abd89184ab44e57
솔 <sol@sol-ui-iMac.local>
2020-06-24 13:47:37 +0900

    Vue.set 사용법

--- src/pages/Next.vue
+++ src/pages/Next.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     Next
-    <div>{{text}}</div>
+    <div>{{members}}</div>
+    <div>{{membersArray}}</div>
+    <button @click="getMembers">버튼</button>
+    <!-- <div>{{text}}</div> -->
     <ComputedSetter :text="'안녕'"></ComputedSetter>
   </div>
 </template>
@@ -12,7 +15,30 @@ import ComputedSetter from "@/components/ComputedSetter";
 export default {
   components: { ComputedSetter },
   data() {
-    return {};
+    return {
+      members: {},
+      membersArray: []
+    };
+  },
+  methods: {
+    getMembers() {
+      const newMember = {
+        name: "Flea",
+        instrument: "Bass",
+        baeLevle: "A++"
+      };
+
+      const myNewValue = "hi";
+
+      //this.members[newMember.name] = newMember;
+      this.$set(this.members, newMember.name, newMember);
+      this.$set(this.membersArray, 0, myNewValue);
+      console.log(this.members);
+      console.log(this.membersArray);
+    }
+  },
+  created() {
+    console.log(this.members);
   }
 };
 </script>
